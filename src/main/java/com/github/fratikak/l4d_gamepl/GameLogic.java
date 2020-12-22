a463272dfa753097fddd68097dfa51ac2207659b
FratikaK <kamunyan17@gmail.com>
2020-12-22 13:04:39 +0900

    スペシャルmob処理追加

--- src/main/java/com/github/fratikak/l4d_gamepl/GameLogic.java
+++ src/main/java/com/github/fratikak/l4d_gamepl/GameLogic.java
@@ -4,11 +4,11 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerToggleSprintEvent;
@@ -32,6 +32,8 @@ public class GameLogic implements Listener {
      * ・サバイバルモードのみスプリント処理を無効化（クリエイティブは可）
      * ・イベント内容がPlayerEventと被る所があるので統一するかもしれない
      * ・敵mobを沸かせる処理を実装する
+     * ・mobスポーンを沸かせる条件に確率でRandom関数を採用しているが、
+     * 　もっといい方法がないか模索する必要がある
      *
      * @author FratikaK
      */
@@ -41,7 +43,7 @@ public class GameLogic implements Listener {
     public void playerDeathEvent(PlayerDeathEvent event) {
         Player player = event.getEntity();
 
-        if (player.getGameMode() == GameMode.SURVIVAL){
+        if (player.getGameMode() == GameMode.SURVIVAL) {
             L4D_gamepl.getPlayerList().remove(player.getDisplayName());
             player.setPlayerListName("[" + ChatColor.RED + "死亡" + ChatColor.WHITE + "]" + player.getDisplayName());
 
@@ -53,16 +55,16 @@ public class GameLogic implements Listener {
 
     //ゲーム参加者のみスプリントを禁止する
     @EventHandler
-    public void dontSplint(PlayerToggleSprintEvent event){
+    public void dontSplint(PlayerToggleSprintEvent event) {
         Player player = event.getPlayer();
 
-        if(player.getGameMode() == GameMode.SURVIVAL && L4D_gamepl.isGame()){
+        if (player.getGameMode() == GameMode.SURVIVAL && L4D_gamepl.isGame()) {
             event.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void zombieSpawn(PlayerMoveEvent event){
+    public void zombieSpawn(PlayerMoveEvent event) {
 
         /**
          * 基本となるゾンビがスポーンするロジックを記述する
@@ -73,20 +75,69 @@ public class GameLogic implements Listener {
 
         Random random = new Random();
         //スポーン確率変更はspawnValueの数値を弄ればOK
-        int spawnValue =  random.nextInt(10);
+        int spawnValue = random.nextInt(10);
 
         Player player = event.getPlayer();
 
         //参加者の周りのみスポーンさせる
-        if (player.getGameMode() == GameMode.SURVIVAL && L4D_gamepl.isGame()){
+        if (player.getGameMode() == GameMode.SURVIVAL && L4D_gamepl.isGame()) {
             Location playerLocation = player.getLocation().clone();
-            Location zombieLocation = playerLocation.add(10,10,10);
+            Location zombieLocation = playerLocation.add(10, 1, 0);
             World world = player.getWorld();
 
-            if(spawnValue == 5){
+            if (spawnValue == 5) {
                 world.spawnEntity(zombieLocation, EntityType.ZOMBIE);
-            }else if(spawnValue == 1){
+            } else if (spawnValue == 1) {
                 world.spawnEntity(zombieLocation, EntityType.ZOMBIE_VILLAGER);
+            }
+        }
+    }
+
+    @EventHandler
+    public void specialMobSpawn(EntityDeathEvent event) {
+
+        /**
+         * ゾンビが死亡すると、一定確率で特殊なMobが出現する
+         * スポーンするMobの挙動は別クラスにて記述する
+         */
+
+        Random random = new Random();
+        int spawnValue = random.nextInt(20);
+
+        EntityType type = event.getEntity().getType();
+
+        if (L4D_gamepl.isGame()) {
+            if (type == EntityType.ZOMBIE || type == EntityType.ZOMBIE_VILLAGER) {
+                Location deathLocation = event.getEntity().getLocation().clone();
+                Location specialLocation = deathLocation.add(20, 1, 0);
+                World world = event.getEntity().getWorld();
+
+                switch (spawnValue) {
+                    case 1:
+                        //ゾンビホース
+                        world.spawnEntity(specialLocation, EntityType.ZOMBIE_HORSE);
+                        break;
+
+                    case 2:
+                        //クリーパー
+                        world.spawnEntity(specialLocation, EntityType.CREEPER);
+                        break;
+
+                    case 3:
+                        //エヴォーカー
+                        world.spawnEntity(specialLocation, EntityType.EVOKER);
+                        break;
+
+                    case 4:
+                        //ラヴェジャー
+                        world.spawnEntity(specialLocation, EntityType.RAVAGER);
+                        break;
+
+                    case 5:
+                        //スライム
+                        world.spawnEntity(specialLocation, EntityType.SLIME);
+                        break;
+                }
             }
         }
     }
---
+++ src/main/java/com/github/fratikak/l4d_gamepl/L4DMobs.java
@@ -0,0 +1,50 @@
+package com.github.fratikak.l4d_gamepl;
+
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+
+public class L4DMobs implements Listener {
+
+    private final L4D_gamepl pl;
+
+    //コンストラクタ
+    public L4DMobs(L4D_gamepl pl) {
+        this.pl = pl;
+    }
+
+    /**
+     * ゲーム中に出現する特殊Mobの処理を記述する
+     * ボス系モンスターもここで作成する
+     *
+     * @author FratikaK
+     */
+
+    //通常敵対しないmobをスポーンさせた場合、プレイヤーに対して敵対化させる
+    @EventHandler
+    public void specialMobLogic(EntitySpawnEvent event) {
+
+        if (L4D_gamepl.isGame()) {
+            Entity entity = event.getEntity();
+            EntityType type = entity.getType();
+            World world = entity.getWorld();
+            LivingEntity livingEntity = (LivingEntity) entity;
+            LivingEntity player = (LivingEntity) world.getPlayers();
+
+            /*
+            今はゾンビホースのみ。今後追加する予定
+            攻撃力の設定が不明
+            EntityDamageByEntityEventで処理するかも
+             */
+            switch (type) {
+                case ZOMBIE_HORSE:
+                    livingEntity.attack(player);
+                    break;
+            }
+        }
+    }
+}
