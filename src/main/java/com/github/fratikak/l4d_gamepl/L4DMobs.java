2bd5d4beb28c694119d67f28af0b3457322e52d8
FratikaK <kamunyan17@gmail.com>
2020-12-23 13:24:53 +0900

    特殊mobの処理を追加

--- src/main/java/com/github/fratikak/l4d_gamepl/L4DMobs.java
+++ src/main/java/com/github/fratikak/l4d_gamepl/L4DMobs.java
@@ -1,12 +1,13 @@
 package com.github.fratikak.l4d_gamepl;
 
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.SlimeSplitEvent;
 import org.bukkit.event.raid.RaidTriggerEvent;
 
 public class L4DMobs implements Listener {
@@ -22,35 +23,33 @@ public class L4DMobs implements Listener {
      * ゲーム中に出現する特殊Mobの処理を記述する
      * ボス系モンスターもここで作成する
      *
+     * ClassCastの問題を解決できない為、ゾンビ馬など通常敵対化しないmobを
+     * モンスターとして登録するのは避けることにする
+     *
      * @author FratikaK
      */
 
-    //通常敵対しないmobをスポーンさせた場合、プレイヤーに対して敵対化させる
+    //クリーパーが攻撃を受けた時に、即爆発するようにする
     @EventHandler
-    public void specialMobLogic(EntitySpawnEvent event) {
+    public void bommer(EntityDamageEvent event){
+        Entity entity = event.getEntity();
 
-        if (L4D_gamepl.isGame()) {
-            Entity entity = event.getEntity();
-            EntityType type = entity.getType();
+        if (entity.getType() == EntityType.CREEPER){
             World world = entity.getWorld();
-            LivingEntity livingEntity = (LivingEntity) entity;
+            Location location = entity.getLocation().clone();
 
-            //ターゲットとなるプレイヤー
-            Entity player = (Entity) world.getPlayers();
-
-            livingEntity.setAI(true);
-
-            /*
-            今はゾンビホースのみ。今後追加する予定
-            攻撃力の設定が不明
-            EntityDamageByEntityEventで処理するかも
-             */
-            switch (type) {
-                case ZOMBIE_HORSE:
-                    livingEntity.attack(player);
-                    break;
-            }
+            //対象の場所に爆発を起こす
+            world.createExplosion(location,3,false,false);
         }
+    }
+
+    //スライムが分裂する度に爆発が起きる
+    @EventHandler
+    public void slimeBommer(SlimeSplitEvent event){
+        World world = event.getEntity().getWorld();
+        Location location = event.getEntity().getLocation();
+
+        world.createExplosion(location,1,false,false);
     }
 
     //村人レイドを発生させない
@@ -58,4 +57,32 @@ public class L4DMobs implements Listener {
     public void notRaid(RaidTriggerEvent event){
         event.setCancelled(true);
     }
+
+//    //通常敵対しないmobをスポーンさせた場合、プレイヤーに対して敵対化させる
+//    @EventHandler
+//    public void specialMobLogic(EntitySpawnEvent event) {
+//
+//        if (L4D_gamepl.isGame()) {
+//            Entity entity = event.getEntity();
+//            EntityType type = entity.getType();
+//            World world = entity.getWorld();
+//            LivingEntity livingEntity = (LivingEntity) entity;
+//
+//            //ターゲットとなるプレイヤー
+//            Entity player = (Entity) world.getPlayers();
+//
+//            livingEntity.setAI(true);
+//
+//            /*
+//            今はゾンビホースのみ。今後追加する予定
+//            攻撃力の設定が不明
+//            EntityDamageByEntityEventで処理するかも
+//             */
+//            switch (type) {
+//                case ZOMBIE_HORSE:
+//                    livingEntity.attack(player);
+//                    break;
+//            }
+//        }
+//    }
 }
