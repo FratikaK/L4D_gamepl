cddbb59cbf03f50df5f76c0083ee7678921093fe
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-01-01 15:08:37 +0900

    コード簡潔に修正

--- src/main/java/com/github/fratikak/l4d_gamepl/Start.java
+++ src/main/java/com/github/fratikak/l4d_gamepl/Start.java
@@ -3,9 +3,6 @@ package com.github.fratikak.l4d_gamepl;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import static java.lang.Thread.sleep;
 
 public class Start {
@@ -61,60 +58,55 @@ public class Start {
             }
         }
 
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
 
-            @Override
-            public void run() {
-                pl.getLogger().info("ゲームに参加するプレイヤーを表示します...");
-                L4D_gamepl.setStarting(true);
-                L4D_gamepl.setTime(0);
-                player.sendMessage("参加者が決まりました");
-                player.sendMessage("参加者は" + ChatColor.AQUA + L4D_gamepl.getPlayerList() + ChatColor.WHITE + "です");
-                player.sendMessage("10秒後に開始します...");
+        pl.getLogger().info("ゲームに参加するプレイヤーを表示します...");
+        L4D_gamepl.setStarting(true);
+        L4D_gamepl.setTime(0);
+        player.sendMessage("参加者が決まりました");
+        player.sendMessage("参加者は" + ChatColor.AQUA + L4D_gamepl.getPlayerList() + ChatColor.WHITE + "です");
+        player.sendMessage("5秒後に開始します...");
 
-                for (int i = 10; i >= 0; i--) {
+        for (int i = 5; i >= 0; i--) {
 
-                    if (!L4D_gamepl.isGame()) {
-                        timer.cancel();
-                    }
+            //プレイヤーにカウントを表示。不参加者にも表示
+            for (Player target : Bukkit.getOnlinePlayers()) {
+                target.playSound(target.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 24);
+                target.sendTitle(ChatColor.WHITE + "ゲーム開始まで" + i + "秒", "", 5, 10, 5);
+                target.sendMessage(ChatColor.GOLD + "ゲーム開始まで" + i + "秒");
 
-                    //プレイヤーにカウントを表示。不参加者にも表示
-                    for (Player target : Bukkit.getOnlinePlayers()) {
-                        target.playSound(target.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 24);
-                        target.sendTitle(ChatColor.WHITE + "ゲーム開始まで" + i + "秒", "", 5, 10, 5);
-                        pl.getServer().broadcastMessage(ChatColor.GOLD + "ゲーム開始まで" + i + "秒");
-                    }
-
-                    try {
-                        sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                L4D_gamepl.setStarting(false);
-                L4D_gamepl.setGame(true);
-
-                //武器などのアイテムを渡す処理
-                for (Player target : Bukkit.getOnlinePlayers()) {
-                    if (target.getGameMode() == GameMode.SURVIVAL) {
-
-                        /*
-                         * <後で記述するもの>
-                         * ステージへテレポート処理
-                         * 参加プレイヤーのステータスを設定
-                         * アイテム付与
-                         */
-                        pl.giveGameItem(target.getInventory(),target);
-                        target.setPlayerListName("[" + ChatColor.AQUA + "生存者" + ChatColor.WHITE + "]" + target.getDisplayName());
-
-                    }
+                if (i == 0) {
+                    target.playSound(target.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 1, 24);
                 }
             }
-        };
-        timer.schedule(task, 0);
+
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        L4D_gamepl.setStarting(false);
+        L4D_gamepl.setGame(true);
+
+        //武器などのアイテムを渡す処理
+        for (Player target : Bukkit.getOnlinePlayers()) {
+            if (target.getGameMode() == GameMode.SURVIVAL) {
+
+                /*
+                 * <後で記述するもの>
+                 * ステージへテレポート処理
+                 * 参加プレイヤーのステータスを設定
+                 * アイテム付与
+                 */
+                pl.giveGameItem(target.getInventory(), target);
+                target.setPlayerListName("[" + ChatColor.AQUA + "生存者" + ChatColor.WHITE + "]" + target.getDisplayName());
+
+            }
+        }
     }
 }
+
+
 
 
