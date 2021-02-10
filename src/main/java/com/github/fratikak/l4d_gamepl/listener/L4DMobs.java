58fc11da83333c011135f6d385f51e959c63d1b5
FratikaK <kamunyan17@gmail.com>
2021-02-10 10:28:02 +0900

    特殊mobの挙動を追加

--- src/main/java/com/github/fratikak/l4d_gamepl/listener/L4DMobs.java
+++ src/main/java/com/github/fratikak/l4d_gamepl/listener/L4DMobs.java
@@ -11,6 +11,14 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.SlimeSplitEvent;
 import org.bukkit.event.raid.RaidTriggerEvent;
 
+/**
+ * ゲーム中に出現する特殊Mobの処理を記述する
+ * ボス系モンスターもここで作成する
+ * ClassCastの問題を解決できない為、ゾンビ馬など通常敵対化しないmobを
+ * モンスターとして登録するのは避けることにする
+ *
+ * @author FratikaK
+ */
 public class L4DMobs implements Listener {
 
     private final L4D_gamepl pl;
@@ -20,38 +28,33 @@ public class L4DMobs implements Listener {
         this.pl = pl;
     }
 
-    /**
-     * ゲーム中に出現する特殊Mobの処理を記述する
-     * ボス系モンスターもここで作成する
-     * <p>
-     * ClassCastの問題を解決できない為、ゾンビ馬など通常敵対化しないmobを
-     * モンスターとして登録するのは避けることにする
-     *
-     * @author FratikaK
-     */
 
-//    //クリーパーが攻撃を受けた時に、即爆発するようにする
-//    @EventHandler
-//    public void bommer(EntityDamageByEntityEvent event) {
-//        Entity entity = event.getEntity();
-//
-//        if (entity.getType() == EntityType.CREEPER) {
-//            World world = entity.getWorld();
-//            Location location = entity.getLocation().clone();
-//
-//            //対象の場所に爆発を起こす
-//            world.createExplosion(location, 3, false, false);
-//        }
-//    }
-//
-//    //スライム（マグマキューブ）が分裂する度に爆発が起きる
-//    @EventHandler
-//    public void slimeBommer(SlimeSplitEvent event) {
-//        World world = event.getEntity().getWorld();
-//        Location location = event.getEntity().getLocation();
-//
-//        world.createExplosion(location, 2, false, false);
-//    }
+    /**
+     * クリーパーが攻撃を受けた時に、即爆発するようにする
+     */
+    @EventHandler
+    public void bommer(EntityDamageByEntityEvent event) {
+        Entity entity = event.getEntity();
+
+        if (entity.getType() == EntityType.CREEPER) {
+            World world = entity.getWorld();
+            Location location = entity.getLocation().clone();
+
+            //対象の場所に爆発を起こす
+            world.createExplosion(location, 3, false, false);
+        }
+    }
+
+    /**
+     * スライム（マグマキューブ）が分裂する度に爆発が起きる
+     */
+    @EventHandler
+    public void slimeBommer(SlimeSplitEvent event) {
+        World world = event.getEntity().getWorld();
+        Location location = event.getEntity().getLocation();
+
+        world.createExplosion(location, 2, false, false);
+    }
 
     //村人レイドを発生させない
     @EventHandler
