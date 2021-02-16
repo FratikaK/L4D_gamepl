3abae153eec1e526dd57b10ea4c61e6cd4244c1d
FratikaK <kamunyan17@gmail.com>
2021-02-16 14:53:16 +0900

    ブロック関係の処理を追加

--- src/main/java/com/github/fratikak/l4d_gamepl/listener/PlayerEvent.java
+++ src/main/java/com/github/fratikak/l4d_gamepl/listener/PlayerEvent.java
@@ -1,24 +1,29 @@
 package com.github.fratikak.l4d_gamepl.listener;
 
 import com.github.fratikak.l4d_gamepl.L4D_gamepl;
-import com.github.fratikak.l4d_gamepl.task.StopTask;
 import com.github.fratikak.l4d_gamepl.util.PerkDecks;
-import org.bukkit.*;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
-import org.bukkit.event.player.*;
-import org.bukkit.metadata.MetadataValue;
+import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.List;
-
+/**
+ * プレイヤー関連のイベントを記述する
+ *
+ * @author FratikaK
+ */
 public class PlayerEvent implements Listener {
 
     private final L4D_gamepl pl;
@@ -29,49 +34,13 @@ public class PlayerEvent implements Listener {
     }
 
     /**
-     * プレイヤー関連のイベントを記述する
-     *
-     * @author FratikaK
-     */
-
-    /**
-     * プレイヤーがログアウトした場合の処理
-     * ゲーム中にログアウトする場合、リストから削除する
-     *
-     * @param event
-     */
-    @EventHandler
-    public void logoutPlayer(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-        event.setQuitMessage(ChatColor.AQUA + player.getDisplayName() + "がログアウトしました");
-        pl.getLogger().info(ChatColor.AQUA + player.getDisplayName() + "がログアウトしました");
-        if (L4D_gamepl.isGame()) {
-            //リストから削除
-            L4D_gamepl.getPlayerList().remove(player);
-            L4D_gamepl.getSurvivorList().remove(player);
-            L4D_gamepl.getDeathPlayerList().remove(player);
-
-            pl.getLogger().info("survivorList: " + L4D_gamepl.getSurvivorList());
-            pl.getLogger().info("deathList: " + L4D_gamepl.getDeathPlayerList());
-
-            //peakの効果は削除
-            new PerkDecks(player, pl).removePotion();
-
-            //ゲームプレイヤーが全員いなくなった場合、ゲームを終了
-            if (L4D_gamepl.getSurvivorList().isEmpty()) {
-                new StopTask(pl).runTaskTimer(pl, 0, 20);
-            }
-        }
-    }
-
-    /**
      * プレイヤーが謎の一撃死バグで死ぬのを防ぐ処理
      * ヴィンディゲーターなど強力なmobからのダメージも軽減する
      *
      * @param event
      */
     @EventHandler
-    public void noOverDamage(EntityDamageEvent event) {
+    public void noOverDamage(EntityDamageByEntityEvent event) {
 
         //落下ダメージは無効
         if (event.getCause() == EntityDamageEvent.DamageCause.FALL) {
@@ -85,33 +54,15 @@ public class PlayerEvent implements Listener {
             return;
         }
         Player player = (Player) entity;
-        //メタデータを持ってるか
-        if (player.hasMetadata(PerkDecks.getPerkKey())) {
 
-            //メタデータはリスト型として返ってくるので、for文で取得する必要がある
-            List<MetadataValue> peeks = player.getMetadata(PerkDecks.getPerkKey());
-
-            MetadataValue value = null;
-
-            for (MetadataValue v : peeks) {
-                if (v.getOwningPlugin().getName() == pl.getName()) {
-                    value = v;
-                    break;
-                }
-            }
-
-            //メタデータが見つからなかった場合はreturn
-            if (value == null) {
-                return;
-            }
-
-            //グラインダーかスカウトであればダメージ増加
-            if (value.asString() == "GRINDER" || value.asString() == "SCOUT") {
-                event.setDamage(6);
-                return;
-            }
+        //付与されているPerkがGrinderかScoutであれば受けるダメージが増える
+        PerkDecks perkDecks = new PerkDecks(player, pl);
+        if (perkDecks.getMetadata(player, PerkDecks.getPerkKey(), pl) == PerkDecks.getGrinder()
+                || perkDecks.getMetadata(player, PerkDecks.getPerkKey(), pl) == PerkDecks.getScout()) {
+            event.setDamage(6);
+        } else {
+            event.setDamage(4);
         }
-        event.setDamage(4);
     }
 
     /**
@@ -132,34 +83,31 @@ public class PlayerEvent implements Listener {
     }
 
     /**
-     * プレイヤーに特定のポーション効果を付与する時、それをキャンセルする
-     * 主にフラッシュバンによるフレンドリーファイア対策
+     * ブロック破壊を無効にする
      *
      * @param event
      */
     @EventHandler
-    public void noPlayerEffect(EntityPotionEffectEvent event) {
+    public void blockBreakEvent(BlockBreakEvent event) {
 
-        Entity entity = event.getEntity();
+        Player player = event.getPlayer();
 
-        if (entity.getType() == EntityType.PLAYER) {
-
-            if (event.getModifiedType() == PotionEffectType.SLOW) {
-                event.setCancelled(true);
-            } else if (event.getModifiedType() == PotionEffectType.BAD_OMEN) {
-                event.setCancelled(true);
-            }
+        if (!player.isOp()) {
+            event.setCancelled(true);
         }
     }
 
     /**
-     * クリエイティブ（観戦者）時にアイテムを拾わせない
+     * ブロック設置を無効にする
+     *
      * @param event
      */
     @EventHandler
-    public void onCreativePickUpItem(PlayerPickupItemEvent event){
+    public void blockPlaceEvent(BlockPlaceEvent event) {
 
-        if (event.getPlayer().getGameMode() == GameMode.CREATIVE){
+        Player player = event.getPlayer();
+
+        if (!player.isOp()) {
             event.setCancelled(true);
         }
     }
