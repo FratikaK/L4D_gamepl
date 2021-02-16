2417e1b4c37605f89a9d7bc7399d3bcb0c3a307c
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-02-16 20:54:15 +0900

    スコアボードのシステムを大幅に変更

--- src/main/java/com/github/fratikak/l4d_gamepl/util/ScoreboardSystem.java
+++ src/main/java/com/github/fratikak/l4d_gamepl/util/ScoreboardSystem.java
@@ -4,8 +4,15 @@ package com.github.fratikak.l4d_gamepl.util;
 import com.github.fratikak.l4d_gamepl.L4D_gamepl;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.*;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * スコアボードのシステムを司るクラス
@@ -15,81 +22,102 @@ import org.bukkit.scoreboard.*;
  */
 public class ScoreboardSystem {
 
-    //キルカウントキー
-    private final static String MOB_KILL_COUNT = "MOB_KILL_COUNT";
+    private List<String> boardLine() {
 
-    //プレイヤーヘルスキー
-    private final static String PLAYER_HEALTH = "PLAYER_HEALTH";
+        if (L4D_gamepl.isGame()) {
 
-    private final L4D_gamepl plugin;
+            //生存者の人数
+            int survive = L4D_gamepl.getSurvivorList().size();
 
-    private final ScoreboardManager manager = Bukkit.getScoreboardManager();
-    private final Scoreboard board = manager.getMainScoreboard();
+            List<String> messageList = new ArrayList<>();
+            messageList.add("");
+            messageList.add("生存者の数" + ChatColor.AQUA + survive + ChatColor.WHITE + "人");
+            messageList.add("");
 
-    public ScoreboardSystem(L4D_gamepl plugin) {
-        this.plugin = plugin;
+            for (Player target : L4D_gamepl.getSurvivorList()) {
+                int health = (int) target.getHealth();
+                int kills = target.getStatistic(Statistic.MOB_KILLS);
+
+                messageList.add(ChatColor.GOLD + "HP" + health + ChatColor.AQUA + target.getDisplayName() + "   " + ChatColor.GOLD + kills);
+            }
+
+            return messageList;
+        }
+        return null;
+    }
+
+    private final Scoreboard scoreBoard = Bukkit.getScoreboardManager().getNewScoreboard();
+
+    public void updateScoreBoard() {
+
+        if (Bukkit.getOnlinePlayers().size() <= 0){
+            return;
+        }
+
+        // Objectiveを取得
+        Objective obj = scoreBoard.getObjective("side");
+
+        // Objectiveが存在しなかった場合は作成
+        if ( obj == null ) {
+            obj = scoreBoard.registerNewObjective("side", "dummy");
+        }
+
+        // Slotを設定
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        obj.setDisplayName(ChatColor.GREEN + "L4DGame");
+
+        // 行を取得
+        List<String> lines = boardLine();
+        // nullが返ってきた場合は非表示にしてreturn
+        if ( lines == null ) {
+            scoreBoard.clearSlot(DisplaySlot.SIDEBAR);
+            return;
+        }
+        // リスト反転
+        Collections.reverse(lines);
+
+        // 現在指定されているEntryを全て解除
+        clearEntries();
+
+        int currentValue = 0;
+        for ( String msg : lines ) {
+
+            // 行が0の場合は空白にする
+            if ( msg == null ) {
+                msg = "";
+            }
+
+            // すでに値が設定されている場合は最後に空白を足していく
+            while ( obj.getScore(msg).isScoreSet() ) {
+                msg = msg + " ";
+            }
+
+            // 値を設定
+            obj.getScore(msg).setScore(currentValue);
+            currentValue++;
+        }
+
+        // スコアボードを設定する
+        Bukkit.getOnlinePlayers().forEach(p -> {
+            if ( p.getScoreboard() != scoreBoard ) {
+                p.setScoreboard(scoreBoard);
+            }
+        });
     }
 
     /**
-     * 新しいキルカウントのスコアボードを作成する
+     * 現在設定されているEntryを全てリセットする
      */
-    public void setKillScoreBoard() {
-
-        //Objectiveがあるか確認、なければ登録する
-        Objective objective = board.getObjective(MOB_KILL_COUNT);
-        if (objective == null) {
-            objective = board.registerNewObjective(MOB_KILL_COUNT, "totalKillCount");
-            objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-            objective.setDisplayName(ChatColor.GREEN + "Kills");
-        }
-
-        //全プレイヤーの現在のキル数を反映(初期は0)
-        for (Player target : L4D_gamepl.getPlayerList()) {
-            objective.getScore(target).setScore(0);
-        }
+    private void clearEntries() {
+        scoreBoard.getEntries().forEach(scoreBoard::resetScores);
     }
 
-    /**
-     * 途中参加プレイヤーのキルカウントスコアボードを追加する
-     *
-     * @param player 　対象のプレイヤー
-     */
-    public void addBoard(Player player) {
+    public void clearSideBar() {
+        // boardがnullでなければSIDEBARを削除
+        scoreBoard.clearSlot(DisplaySlot.SIDEBAR);
 
-        //Objectiveがあるのが前提
-        Objective objective = board.getObjective(MOB_KILL_COUNT);
-
-        //後参加のプレイヤーの初期値を設定
-        objective.getScore(player).setScore(0);
-
-        //現在のスコアボードを更新する
-        for (Player target : L4D_gamepl.getPlayerList()) {
-            objective.getScore(target).setScore(objective.getScore(target).getScore());
-        }
-    }
-
-    /**
-     * プレイヤーの体力を表示するスコアボード
-     * ログインしてきた時に使用する
-     */
-    public void viewHealthBoard() {
-
-        //キルカウントでメインのスコアボードを使用しているので
-        //新しくここでScoreboardを宣言する
-        ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
-        Scoreboard scoreboard = scoreboardManager.getNewScoreboard();
-
-        //Objectiveがなければ新規に作成する
-        Objective objective = scoreboard.getObjective(PLAYER_HEALTH);
-        if (objective == null) {
-            objective = scoreboard.registerNewObjective(PLAYER_HEALTH, "health");
-            objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
-            objective.setDisplayName("HP");
-        }
-
-        //全プレイヤーの現在の体力を反映
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            objective.getScore(player).setScore((int) player.getHealth());
+        for (Player target : Bukkit.getOnlinePlayers()){
+            target.setStatistic(Statistic.MOB_KILLS,0);
         }
     }
 }
